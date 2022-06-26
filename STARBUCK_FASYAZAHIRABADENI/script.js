6787e51e4664c18017a3fa45c07da486e7bf87e5
FasyaZahira20 <108218510+FasyaZahira20@users.noreply.github.com>
2022-06-26 17:25:50 +0700

    Add files via upload

---
+++ STARBUCK_FASYAZAHIRABADENI/script.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+function imgslider (anything) {
+    document.querySelector ('Starbuck').src= anything
+}
+
+function changeCircleColor(color){
+circle = document.querySelector('.circle')}
+circle.style.background = color;
+
+</script>
